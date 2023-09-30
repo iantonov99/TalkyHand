af5b0d6df995001c6ce5f55e55148f721b766823
Sibilla Silbano <sibi.silbano99@gmail.com>
2023-09-30 22:23:05 +0200

    UI edit

---
+++ textToSpeech.py
@@ -0,0 +1,8 @@
+# pip3 install gTTS playsound
+
+import gtts
+from playsound import playsound
+
+tts = gtts.gTTS("Hello world")
+tts.save("hello.mp3")
+playsound("hello.mp3")
--- userInterface.py
+++ userInterface.py
@@ -1,50 +1,34 @@
-# to install: pip3 install tk / tkinter_webcam / opencv-python / Pillow
+# To install: pip3 install tk / tkinter_webcam / opencv-python / Pillow
 import tkinter as tk
 from tkinter_webcam import webcam
+import tkinter.scrolledtext as ScrolledText
 
-window = tk.Tk() # Create a window
-window.title("TalkyHand") # Set the title of the window
-window.geometry("500x500") # Set the size of the window
+# Create a window
+window = tk.Tk()
+window.title("TalkyHand")
+window.geometry("1000x800")
 
-# webcame code 
+# Webcam code
 video = webcam.Box(window, width=500, height=500)
-video.show_frames() # show the created box 
+video.show_frames()
 
-tk.mainloop() # Start the main loop
+# Example chat to implement
+text_widget = ScrolledText.ScrolledText(window)
+text_widget.pack(fill='both', expand=True)
 
-""""
+def send_message():
+    message = entry.get()
+    if message:
+        text_widget.insert('end', '\nYou: ' + message)
+        entry.delete(0, 'end')
 
-# ALTERNATIVE 2
+# Entry widget for user input
+entry = tk.Entry(window, font=("Helvetica", 14))
+entry.pack(fill='x', pady=10)
+entry.bind('<Return>', lambda event=None: send_message())
 
-# Import required Libraries
-from tkinter import *
-from PIL import Image, ImageTk
-import cv2
+send_button = tk.Button(window, text="Send", command=send_message)
+send_button.pack()
 
-# Create an instance of TKinter Window or frame
-win = Tk()
-
-# Set the size of the window
-win.geometry("700x350")
-
-# Create a Label to capture the Video frames
-label =Label(win)
-label.grid(row=0, column=0)
-cap= cv2.VideoCapture(0)
-
-# Define function to show frame
-def show_frames():
-   # Get the latest frame and convert into Image
-   cv2image= cv2.cvtColor(cap.read()[1],cv2.COLOR_BGR2RGB)
-   img = Image.fromarray(cv2image)
-   # Convert image to PhotoImage
-   imgtk = ImageTk.PhotoImage(image = img)
-   label.imgtk = imgtk
-   label.configure(image=imgtk)
-   # Repeat after an interval to capture continiously
-   label.after(20, show_frames)
-
-show_frames()
-win.mainloop()
-
-"""
+# Start the main loop
+window.mainloop()
