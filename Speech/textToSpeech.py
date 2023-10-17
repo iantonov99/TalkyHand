7770070c809be0b4887ac301e6101846ca4e8b1b
Sibilla Silbano <sibi.silbano99@gmail.com>
2023-10-18 00:54:23 +0200

    Text-to-Speech

--- Speech/textToSpeech.py
+++ Speech/textToSpeech.py
@@ -4,9 +4,10 @@ from playsound import playsound
 # Specify the text, language, and gender
 text = "Hello world"
 language = 'en'  # Language code for English
+accent = 'us'
 
 # Create the gTTS object with speed control and male voice
-tts = gtts.gTTS(text, lang=language, slow=False)
+tts = gtts.gTTS(text, lang=language, slow=False, tld=accent)
 
 # Save the TTS output to a file
 tts.save("Readable.mp3")
