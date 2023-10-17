c16a82f70ee7a393f88a6cef0ed9d09f2a1e0c78
Benedart <49732385+Benedart@users.noreply.github.com>
2023-10-18 01:17:54 +0200

    Added textToSpeech

--- userInterface.py
+++ userInterface.py
@@ -1,4 +1,3 @@
-from math import pi
 import tkinter as tk
 import customtkinter
 import csv
@@ -17,6 +16,9 @@ import threading
 
 from chatSender import ChatSender
 import socket
+
+import gtts
+from playsound import playsound
 
 load_dotenv()
 
@@ -666,7 +668,30 @@ def sendToChat(text):
 
 
 def labelClicked(text):
-    print("It's working " + text)
+    try:
+        # Specify the text, language, and gender
+        language = "en"  # Language code for English
+        accent = "us"
+
+        # Create the gTTS object with speed control and male voice
+        tts = gtts.gTTS(text, lang=language, slow=False, tld=accent)
+
+        # Save the TTS output to a file
+        tts.save("Readable.mp3")
+
+        # Play the generated audio on a separate thread
+        audio_thread = threading.Thread(target=playAudio)
+        audio_thread.Daemon = True
+        audio_thread.start()
+    except Exception as e:
+        print(f"Failed to play audio: {e}")
+
+
+def playAudio():
+    playsound("Readable.mp3")
+
+    # Delete the file
+    os.remove("Readable.mp3")
 
 
 class SpeechListener:
